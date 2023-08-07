0cce46a1de75c1c19171e09a2d0b7767ffe31ecf
GJZ26 <adolfogj26@gmail.com>
2023-08-07 09:51:13 -0600

    first commit

---
+++ fbposter.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: FB Poster
+Plugin URI: https://github.com/GJZ26/FB-Poster
+Description: A wordpress plugin that automates posts made from wordpress :)
+Version: 0.0.0
+Author: GJZ26
+Author URI: https://github.com/GJZ26
+*/
